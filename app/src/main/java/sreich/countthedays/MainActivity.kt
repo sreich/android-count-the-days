006a27e77e50b71ca08e22ad9e408f4e849d353d
Shaun Reich <sreich02@gmail.com>
2016-10-09 19:09:07 -0400

    beginning of passing data around

--- app/src/main/java/sreich/countthedays/MainActivity.kt
+++ app/src/main/java/sreich/countthedays/MainActivity.kt
@@ -6,12 +6,16 @@ import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.text.format.DateUtils
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    val counterList = mutableListOf<DayCounter>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,20 +25,30 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         val fab = findViewById(R.id.fab) as FloatingActionButton
-        fab.setOnClickListener { view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show()
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show()
             startActivity(Intent(this, SettingsActivity::class.java))
         }
 
-        val counterList = mutableListOf<DayCounter>()
         repeat(50) {
-                counterList.add(DayCounter("name number $it"))
+            counterList.add(DayCounter("name number $it"))
         }
 
         val listView = findViewById(R.id.listview) as ListView
         listView.adapter = DayCounterAdapter(this, counterList)
 
         listView.onItemClickListener = AdapterView.OnItemClickListener() { adapterView: AdapterView<*>, view: View, position: Int, id: Long ->
-            startActivity(Intent(this, NewCounterActivity::class.java))
+            val intent = Intent(this, NewCounterActivity::class.java)
+
+            val currentCounter = counterList[position]
+            intent.putExtra("name", currentCounter.name)
+
+            val c = Calendar.getInstance()
+            c.set(2050, 1, 2)
+
+            intent.putExtra("calendar", c)
+
+            startActivity(intent)
         }
     }
 
--- app/src/main/java/sreich/countthedays/NewCounterActivity.kt
+++ app/src/main/java/sreich/countthedays/NewCounterActivity.kt
@@ -1,6 +1,7 @@
 package sreich.countthedays
 
 import android.app.DatePickerDialog
+import android.icu.text.DateFormat
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
@@ -9,6 +10,8 @@ import android.support.v7.widget.Toolbar
 import android.view.View
 import android.widget.Button
 import android.widget.DatePicker
+import android.widget.EditText
+import java.util.*
 
 class NewCounterActivity : AppCompatActivity() {
 
@@ -16,20 +19,22 @@ class NewCounterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_new_counter)
-
         val toolbar = findViewById(R.id.toolbar) as Toolbar
 
         setSupportActionBar(toolbar)
 
-
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        val nameText = findViewById(R.id.nameText) as EditText
+        nameText.setText(intent.getStringExtra("name"))
+
+        val currentCalendar = intent.getSerializableExtra("calendar") as Calendar
 
         val dateDialog = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { datePicker, year, month, day ->
             datePicked(datePicker, year, month, day)
-        }, 0, 0, 0)
+        }, currentCalendar.get(Calendar.YEAR), currentCalendar.get(Calendar.MONTH), currentCalendar.get(Calendar.DATE))
 
-
-        val button = findViewById(R.id.button2) as Button
+        val button = findViewById(R.id.dateText) as EditText
         button.setOnClickListener {
             dateDialog.show()
         }
@@ -37,7 +42,6 @@ class NewCounterActivity : AppCompatActivity() {
     }
 
     private fun datePicked(datePicker: DatePicker, year: Int, month: Int, day: Int) {
-        TODO()
     }
 
 }
