760c3cb803f6d04bf7dea8cfceb860a79b492f9e
Shaun Reich <sreich02@gmail.com>
2016-10-10 17:34:55 -0400

    view displaying text pulled from model now
    albeit stupidly, but probably wouldn't be an issue for this app anyway

--- app/src/main/java/sreich/countthedays/DayCounterAdapter.kt
+++ app/src/main/java/sreich/countthedays/DayCounterAdapter.kt
@@ -1,10 +1,13 @@
 package sreich.countthedays
 
 import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.RecyclerView.ViewHolder
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
 
 /**
  * Created by sreich on 10/9/16.
@@ -19,9 +22,46 @@ class DayCounterAdapter(context: Context, val counterList: List<DayCounter>) : B
 
     override fun getCount() = counterList.size
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val rowView = inflater.inflate(R.layout.list_item_daycounter, parent, false)
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View? {
+        var holder: DayViewHolder? = null
+        var convertView2: View? = null
+        convertView2 = inflater.inflate(R.layout.list_item_daycounter, parent, false)
+//
+//        if (convertView == null) {
+//            convertView2 = inflater.inflate(R.layout.list_item_daycounter, parent, false)
+//
+//            val holder = DayViewHolder()
+//            holder.nameTextView = convertView2.findViewById(R.id.name_text_view) as TextView
+//            holder.dateTextView = convertView2.findViewById(R.id.time_text_view) as TextView
+//
+//            assert (holder != null)
+//            assert(convertView2 != null)
+//            convertView2.setTag(holder)
+//        } else {
+//            holder = convertView.tag as DayViewHolder
+//        }
+        holder = DayViewHolder()
 
-        return rowView
+        convertView2.toString()
+
+        val a = convertView2.findViewById(R.id.name_text_view)
+        val b = a as TextView
+
+        holder!!.nameTextView = convertView2.findViewById(R.id.name_text_view) as TextView
+        holder!!.dateTextView = convertView2.findViewById(R.id.time_text_view) as TextView
+        val nameTextView = holder!!.nameTextView
+
+        val dayCounter = getItem(position)
+        nameTextView.text = dayCounter.name
+
+        val dateTextView = holder.dateTextView
+        dateTextView.text = "test date text"
+
+        return convertView2
+    }
+
+    private class DayViewHolder {
+        lateinit var nameTextView: TextView
+        lateinit var dateTextView: TextView
     }
 }
